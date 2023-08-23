fd82e3bdd66d7213ea898fd0c68cdf5776d3af08
KaseyHaman <141760608+KaseyHaman@users.noreply.github.com>
2023-08-23 13:22:26 -0700

    Hw2

--- MAE40/Hw2.m
+++ MAE40/Hw2.m
@@ -1,13 +1,13 @@
 clear all;
 clc
-RRbase='C:\Users\kasey\Documents\GitHub\RR'; cd(RRbase); RR_path_init
-cd 'C:\Users\kasey\Documents\GitHub\UCSD\MAE40'
+RRbase='/Users/kasey/RR'; cd(RRbase); RR_path_init
+cd '/Users/kasey/Desktop/School Related/UCSD/MAE40'
 
 %%Problem 1
 syms s Vo V1 V2 Cc Cb Rd Ra
 % x={I_o, I_a, I_b, I_c, I_d, V1 V2}  <-- unknown vector
 A  =[ 1 -1 -1 0 0 0 0;         % Io = Ia + Ib 
-     1 -1 0 -1 0 0 0;          % Io = Ia + Ic
+     0 -1 0 -1 0 0 0;          % 0 = Ia + Ic
      0 0 1 -1 -1 0 0;          % Ib = Id + Ic
      0 Ra 0 0 0 1 0;           % Vo = V1 + Ia *Ra
      0 0 1/(Cb*s) 0 0 0 1;     % Ib / (Cb * s) + V2 = Vo
@@ -16,11 +16,18 @@ A  =[ 1 -1 -1 0 0 0 0;         % Io = Ia + Ib
 b  =[ 0; 0; 0; Vo; Vo; 0; 0];
 x=A\b; Vo_LPF2_P1=simplify(x(6))
 
+%BODE Plot
+omega1 = 10;
+zeta = 5;
+P1bode = RR_tf([1, omega1, omega1.^2], [1, 2*omega1*zeta, omega1.^2]);
+figure(1), RR_bode(P1bode)
+
+
 %%Problem 2
 syms s Vo V1 V2 Ca Rb Rc Cd
 % x={I_o, I_a, I_b, I_c, I_d, V1 V2}  <-- unknown vector
 A  =[1 -1 -1 0 0 0 0;          % Io = Ia + Ib 
-     1 -1 0 -1 0 0 0;          % Io = Ia + Ic
+     0 -1 0 -1 0 0 0;          % 0 = Ia + Ic
      0 0 1 -1 -1 0 0;          % Ib = Id + Ic
      0 1/(Ca*s) 0 0 0 1 0;     % Vo = Ia/(Ca*s) + V1
      0 0 Rb 0 0 0 1;           % Vo = Ib*Rb + V2
@@ -29,3 +36,8 @@ A  =[1 -1 -1 0 0 0 0;          % Io = Ia + Ib
 b  =[ 0; 0; 0; Vo; Vo; 0; 0];
 x=A\b; Vo_LPF2_P2=simplify(x(6))
 
+%BODE Plot
+omega1 = 10;
+zeta = 3/2;
+P2bode = RR_tf([1, omega1, omega1.^2], [1, 2*omega1*zeta, omega1.^2]);
+figure(2), RR_bode(P2bode)
